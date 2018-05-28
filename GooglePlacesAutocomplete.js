502bada4a1193025b561abc2373cf43cede1664c
Valeria Shpiner <shpiner.v@avocadotechnology.com>
2018-05-28 15:59:16 +0300

    test commit

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -568,6 +568,7 @@ export default class GooglePlacesAutocomplete extends Component {
           onPress={() => this._onPress(rowData)}
           underlayColor={this.props.listUnderlayColor || "#c8c7cc"}
         >
+        <Text>Test</Text>
           <View style={[this.props.suppressDefaultStyles ? {} : defaultStyles.row, this.props.styles.row, rowData.isPredefinedPlace ? this.props.styles.specialItemRow : {}]}>
             {this._renderRowData(rowData)}
             {this._renderLoader(rowData)}
